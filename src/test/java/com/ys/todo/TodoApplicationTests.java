f9a82fa90ea9f2b701e1a50c10b7bc7154efc907
seino_ta <seino_ta@works-hi.co.jp>
2022-04-08 18:23:47 +0900

    first commit

---
+++ src/test/java/com/ys/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
