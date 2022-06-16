92126f5d70a37473dc1a99fcb89e64543656463e
seino_ta <seino_ta@works-hi.co.jp>
2022-06-16 14:17:11 +0900

    Fix build but error in browser is exists.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,70 +1,64 @@
 'use strict';
 
-// tag::vars[]
-const React = require('react'); // <1>
-const ReactDOM = require('react-dom'); // <2>
-const client = require('./client'); // <3>
-// end::vars[]
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
 
-// tag::app[]
-class App extends React.Component { // <1>
+
+class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {employees: []};
+		this.state = {todos: []};
 	}
 
-	componentDidMount() { // <2>
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
+	componentDidMount() {
+		client({method: 'GET', path: '/api/todos'}).done(response => {
+			this.setState({todos: response.entity._embedded.todos});
 		});
 	}
 
-	render() { // <3>
+	render() {
 		return (
-			<EmployeeList employees={this.state.employees}/>
+			<TodoList todos={this.state.todos}/>
 		)
 	}
 }
-// end::app[]
 
-// tag::employee-list[]
-class EmployeeList extends React.Component{
+class TodoList extends React.Component{
 	render() {
-		const employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
+		const todos = this.props.todos.map(todo =>
+			<Todo key={todo._links.self.href} todo={todo}/>
 		);
 		return (
 			<table>
 				<tbody>
 					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
+						<th>Name</th>
+						<th>Overview</th>
+						<th>Content</th>
+						<th>Status</th>
 					</tr>
-					{employees}
+					{todos}
 				</tbody>
 			</table>
 		)
 	}
 }
-// end::employee-list[]
 
-// tag::employee[]
-class Employee extends React.Component{
+class Todo extends React.Component{
 	render() {
 		return (
 			<tr>
-				<td>{this.props.employee.firstName}</td>
-				<td>{this.props.employee.lastName}</td>
-				<td>{this.props.employee.description}</td>
+				<td>{this.props.todo.name}</td>
+				<td>{this.props.todo.overview}</td>
+				<td>{this.props.todo.content}</td>
+                <td>{this.props.todo.status}</td>
 			</tr>
 		)
 	}
 }
-// end::employee[]
 
-// tag::render[]
 ReactDOM.render(
 	<App />,
 	document.getElementById('react')
