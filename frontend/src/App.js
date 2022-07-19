baf0e16827fa4b4c0219db58353823b6fcbfcae2
yuuki121 <yuuki_kind@icloud.com>
2022-07-19 12:27:07 +0900

    パート2ソース内容反映

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
 
+const root = '/api';
+
 import './main.css';
 import 'bulma/css/bulma.css';
 
@@ -12,26 +14,154 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {todos: []};
+		this.updatePageSize = this.updatePageSize.bind(this);
+		this.onCreate = this.onCreate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
+		this.onNavigate = this.onNavigate.bind(this);
 	}
 
 	componentDidMount() {
-		client({method: 'GET', path: '/api/todos'}).done(response => {
-			this.setState({todos: response.entity._embedded.todos});
-		});
+		this.loadFromServer(this.state.pageSize);
 	}
 
 	render() {
 		return (
+			<div>
+			<CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
 			<TodoList todos={this.state.todos}/>
+			</div>
 		)
+	}
+
+	loadFromServer(pageSize) {
+		follow(client, root, [
+			{rel: 'todos', params: {size: pageSize}}]
+		).then(todoCollection => {
+			return client({
+				method: 'GET',
+				path: todoCollection.entity._links.profile.href,
+				headers: {'Accept': 'application/schema+json'}
+			}).then(schema => {
+				this.schema = schema.entity;
+				return todoCollection;
+			});
+		}).done(todoCollection => {
+			this.setState({
+				todos: todoCollection.entity._embedded.todos,
+				attributes: Object.keys(this.schema.properties),
+				pageSize: pageSize,
+				links: todoCollection.entity._links});
+		});
+	}
+
+	
+onCreate(newTodo) {
+	follow(client, root, ['todos']).then(todoCollection => {
+		return client({
+			method: 'POST',
+			path: todoCollection.entity._links.self.href,
+			entity: newTodo,
+			headers: {'Content-Type': 'application/json'}
+		})
+	}).then(() => {
+		return follow(client, root, [
+			{rel: 'todos', params: {'size': this.state.pageSize}}]);
+	}).done(response => {
+		if (typeof response.entity._links.last !== "undefined") {
+			this.onNavigate(response.entity._links.last.href);
+		} else {
+			this.onNavigate(response.entity._links.self.href);
+		}
+	});
+}
+
+onNavigate(navUri) {
+	client({method: 'GET', path: navUri}).done(todoCollection => {
+		this.setState({
+			todos: todoCollection.entity._embedded.todos,
+			attributes: this.state.attributes,
+			pageSize: this.state.pageSize,
+			links: todoCollection.entity._links
+		});
+	});
+}
+
+onDelete(todo) {
+	client({method: 'DELETE', path: todo._links.self.href}).done(() => {
+		this.loadFromServer(this.state.pageSize);
+	});
+}
+
+updatePageSize(pageSize) {
+	if (pageSize !== this.state.pageSize) {
+		this.loadFromServer(pageSize);
 	}
 }
 
+}
+
 class TodoList extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.myrefs = React.createRef();
+		this.handleNavFirst = this.handleNavFirst.bind(this);
+		this.handleNavPrev = this.handleNavPrev.bind(this);
+		this.handleNavNext = this.handleNavNext.bind(this);
+		this.handleNavLast = this.handleNavLast.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+	}
+
+	handleInput(e) {
+		e.preventDefault();
+		const pageSize = ReactDOM.findDOMNode(this.myrefs.pageSize).value;
+		if (/^[0-9]+$/.test(pageSize)) {
+			this.props.updatePageSize(pageSize);
+		} else {
+			ReactDOM.findDOMNode(this.myrefs.pageSize).value =
+				pageSize.substring(0, pageSize.length - 1);
+		}
+	}
+
+	handleNavFirst(e){
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.first.href);
+	}
+	
+	handleNavPrev(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.prev.href);
+	}
+	
+	handleNavNext(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.next.href);
+	}
+	
+	handleNavLast(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.last.href);
+	}
+	
 	render() {
 		const todos = this.props.todos.map(todo =>
 			<Todo key={todo._links.self.href} todo={todo}/>
 		);
+
+		const navLinks = [];
+		if ("first" in this.props.links) {
+			navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+		}
+		if ("prev" in this.props.links) {
+			navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+		}
+		if ("next" in this.props.links) {
+			navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+		}
+		if ("last" in this.props.links) {
+			navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+		}
+
 		return (
 			<div>
 				<div className="tile is-vertical">
@@ -67,6 +197,14 @@ class TodoList extends React.Component{
 }
 
 class Todo extends React.Component{
+	constructor(props) {
+		super(props);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+
+	handleDelete() {
+		this.props.onDelete(this.props.todo);
+	}
 	render() {
 		return (
 			<tr>
@@ -74,10 +212,69 @@ class Todo extends React.Component{
 				<td>{this.props.todo.overview}</td>
 				<td>{this.props.todo.content}</td>
 				<td>{this.props.todo.status}</td>
+				<td>
+					<button onClick={this.handleDelete}>Delete</button>
+				</td>
 			</tr>
 		)
 	}
 }
+
+class CreateDialog extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.myrefs = React.createRef();
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const newTodo = {};
+		this.props.attributes.forEach(attribute => {
+			newTodo[attribute] = ReactDOM.findDOMNode(this.myrefs[attribute]).value.trim();
+		});
+		this.props.onCreate(newTodo);
+
+		// clear out the dialog's inputs
+		this.props.attributes.forEach(attribute => {
+			ReactDOM.findDOMNode(this.myrefs[attribute]).value = '';
+		});
+
+		// Navigate away from the dialog to hide it.
+		window.location = "#";
+	}
+
+	render() {
+		const inputs = this.props.attributes.map(attribute =>
+			<p key={attribute}>
+				<input type="text" placeholder={attribute} ref={attribute} className="field"/>
+			</p>
+		);
+
+		return (
+			<div>
+				<a href="#createTodo">Create</a>
+
+				<div id="createTodo" className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+
+						<h2>Create new todo</h2>
+
+						<form>
+							{inputs}
+							<button onClick={this.handleSubmit}>Create</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+
 
 ReactDOM.render(
 	<App />,
